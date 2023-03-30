215dcb3bd9cac2feb41fc8ffe333373fb21e7bf2
Andrey Kharginen <kut13@inbox.ru>
2023-03-30 20:30:16 +0400

    add code and test gamestore

--- src/main/java/GameStore.java
+++ src/main/java/GameStore.java
@@ -11,7 +11,7 @@ public class GameStore {
      * Ключ - имя игрока
      * Значение - суммарное количество часов в игры этого каталога
      */
-    private Map<String, Integer> playedTime = new HashMap<>();
+    public Map<String, Integer> playedTime = new HashMap<>();
 
     /**
      * Создание объекта игры с заданными заголовком и жанром
@@ -28,8 +28,8 @@ public class GameStore {
      * если игра есть и false иначе
      */
     public boolean containsGame(Game game) {
-        for (int i = 1; i < games.size(); i++) {
-            if (games.get(i - 1).equals(game)) {
+        for (int i = 0; i < games.size(); i++) {
+            if (games.get(i).equals(game)) {
                 return true;
             }
         }
@@ -42,23 +42,23 @@ public class GameStore {
      * суммироваться с прошлым значением для этого игрока
      */
     public void addPlayTime(String playerName, int hours) {
-        if (playedTime.containsKey(playerName)) {
-            playedTime.put(playerName, playedTime.get(playerName));
-        } else {
-            playedTime.put(playerName, hours);
+            if (playedTime.containsKey(playerName)) {
+                playedTime.put(playerName, playedTime.get(playerName) + hours);
+            } else {
+                playedTime.put(playerName, hours);
+            }
         }
-    }
 
     /**
      * Ищет имя игрока, который играл в игры этого каталога больше всего
      * времени. Если игроков нет, то возвращется null
      */
     public String getMostPlayer() {
-        int mostTime = 1;
+        int mostTime = 0;
         String bestPlayer = null;
         for (String playerName : playedTime.keySet()) {
             int playerTime = playedTime.get(playerName);
-            if (playerTime > mostTime) {
+            if (playerTime >= mostTime) {
                 mostTime = playerTime;
                 bestPlayer = playerName;
             }
@@ -71,6 +71,10 @@ public class GameStore {
      * за играми этого каталога
      */
     public int getSumPlayedTime() {
-        return 0;
+        int sum = 0;
+        for (String playerName : playedTime.keySet()) {
+            sum += playedTime.get(playerName);
+        }
+        return sum;
     }
 }
--- src/test/java/GameStoreTest.java
+++ src/test/java/GameStoreTest.java
@@ -1,10 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
     @Test
-    public void shouldAddGame() {
+    public void addGame() {
 
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
@@ -12,5 +13,98 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldAddGameIfZero() {
+
+        GameStore store = new GameStore();
+        Game game = new Game("Нетология Баттл Онлайн", "Аркады", store);
+
+        assertFalse(store.containsGame(game));
+    }
+
+    @Test
+    public void shouldAddGamesIfSeveral() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Dota2", "Moba");
+
+        assertTrue(store.containsGame(game));
+        assertTrue(store.containsGame(game2));
+    }
+
+    @Test
+    public void shouldAddPlayerTimeFirstTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        int expected = 1;
+        int actual = store.playedTime.get("Player1");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddPlayerTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        store.addPlayTime("Player1", 2);
+        int expected = 3;
+        int actual = store.playedTime.get("Player1");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetMostPlayer1() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        String expected = "Player1";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shoulLongestTimePlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        store.addPlayTime("Player2", 3);
+        store.addPlayTime("Player3", 2);
+        String expected = "Player2";
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldWithoutPlayers() {
+        GameStore store = new GameStore();
+        String expected = null;
+        String actual = store.getMostPlayer();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetSumPlayedTime() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        store.addPlayTime("Player2", 3);
+        store.addPlayTime("Player3", 2);
+        int expected = 6;
+        int actual = store.getSumPlayedTime();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetSumPlayedTimeIfFirstPlayer() {
+        GameStore store = new GameStore();
+        store.addPlayTime("Player1", 1);
+        int expected = 1;
+        int actual = store.getSumPlayedTime();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetSumPlayTimeWithoutPlayers() {
+        GameStore store = new GameStore();
+        int expected = 0;
+        int actual = store.getSumPlayedTime();
+        assertEquals(expected, actual);
+    }
 }
